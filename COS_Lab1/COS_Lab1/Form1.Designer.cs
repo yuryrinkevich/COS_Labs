c087d5d0f98e9175bd89307d8e1eafab2d8e4f05
yuryrinkevich <rinkevich03@gmail.com>
2020-10-23 23:50:57 +0300

    first laba

---
+++ COS_Lab1/COS_Lab1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace COS_Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Blue;
+            this.button1.Location = new System.Drawing.Point(1113, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 90);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1059, 600);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(1113, 440);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 90);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "EXIT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1337, 747);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ COS_Lab1/COS_Lab1/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace COS_Lab1
+{
+    public partial class Form1 : Form
+    {
+        public String bfType;
+        public Int32 bfSize;
+        public Int16 bfReserved1;
+        public Int16 bfreserved2;
+        public Int32 bfOffBits;
+        public Int32 bfSizeheader;
+        public Int32 bfShirinaImage;
+        public Int32 bfVisotaImage;
+        public Int16 bfNumberPlosk;
+        public Int16 bfBitPixel;
+        public Int32 bfCompress;
+        public Int32 bfSizeRastMass;
+        public Int32 bfGorSize;
+        public Int32 bfVertSize;
+        public Int32 bfNumberColors;
+        public Int32 bfMainColors;
+
+        public Form1()
+        {
+            InitializeComponent();
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "bmp |*.bmp";
+            openFileDialog1.ShowDialog();
+            BinaryReader bReader = new BinaryReader(File.Open(openFileDialog1.FileName, FileMode.Open));
+            bfType = new string(bReader.ReadChars(2));
+            bfSize = bReader.ReadInt32();
+            bfReserved1 = bReader.ReadInt16();
+            bfreserved2 = bReader.ReadInt16();
+            bfOffBits = bReader.ReadInt32();
+            bfSizeheader = bReader.ReadInt32();
+            bfShirinaImage = bReader.ReadInt32();
+            bfVisotaImage = bReader.ReadInt32();
+            bfNumberPlosk = bReader.ReadInt16();
+            bfBitPixel = bReader.ReadInt16();
+            bfCompress = bReader.ReadInt32();
+            bfSizeRastMass = bReader.ReadInt32();
+            bfGorSize = bReader.ReadInt32();
+            bfVertSize = bReader.ReadInt32();
+            bfNumberColors = bReader.ReadInt32();
+            bfMainColors = bReader.ReadInt32();
+
+
+            bReader.Close();
+
+            String CompressType = 0.ToString();
+            if (bfCompress == 0 || bfCompress == 3 || bfCompress == 6)
+                CompressType = "Без сжатия";
+            else if (bfCompress == 1 || bfCompress == 2)
+                CompressType = "RLE";
+            else if (bfCompress == 4)
+                CompressType = "JPEG";
+            else if (bfCompress == 5)
+                CompressType = "PNG";
+
+            Bitmap original_image = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = original_image;
+            pictureBox1.Show();
+
+
+            String message = "Сигнатура файла: " + bfType + "\n Размер файла: " + bfSize.ToString() +
+                             "\n Местонахождение данных растрового массива: " + bfOffBits.ToString() +
+                             "\n Длина заголовка растрового массива: " + bfSizeheader.ToString() +
+                             "\n Ширина изобрадения: " + bfShirinaImage.ToString() + "\n Высота изображения: " +
+                             bfVisotaImage.ToString() + "\n Число цевтовых плоскостей: " + bfNumberPlosk +
+                             "\n Бит/пиксел: " + bfBitPixel + "\n Метод сжатия: " + CompressType +
+                             "\n Длина растрового массива: " + bfSizeRastMass + "\n Горизонтальное разрешение: " +
+                             bfGorSize + "\n Вертикальное разрешение: " + bfVertSize +
+                             "\n Количество цветов изображения: " + bfNumberColors + "\n Количество основных цветов: " +
+                             bfMainColors;
+
+            MessageBox.Show(message);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
